bbcedf2d7d57db30b1789821347bfc0a17395ce2
Analice Ferreira <ferreiraanalice122@gmail.com>
2026-03-24 00:00:31 -0300

    implementa autenticação JWT e criptografia de senhas

--- src/Application/Controllers/user_controller.py
+++ src/Application/Controllers/user_controller.py
@@ -34,3 +34,33 @@ class UserController:
             return make_response(jsonify({"mensagem": "Vendedor ativado com sucesso"}), 200)
         else:
             return make_response(jsonify({"erro": "Código inválido ou vendedor já ativo"}), 400)
+        
+    @staticmethod
+    def login_user():
+        try:
+            data = request.get_json()
+           
+            # Pegando os campos exatos do curl do professor
+            email = data.get('email')
+            senha = data.get('senha')
+           
+            if not email or not senha:
+                 return jsonify({"error": "E-mail e senha são obrigatórios"}), 400
+
+            # O Controller chama a regra de negócio do Service
+            token = UserService.login(email, senha)
+           
+            return jsonify({"message": "Login efetuado com sucesso!", "token": token}), 200
+           
+        except ValueError as ve:
+            # Tratamento de erro 1: Senha ou email errados (Erro 401)
+            return jsonify({"error": str(ve)}), 401
+           
+        except PermissionError as pe:
+            # Tratamento de erro 2: Usuário inativo no zap (Erro 403)
+            return jsonify({"error": str(pe)}), 403
+           
+        except Exception as e:
+            # Tratamento de erro 3: Evita que o servidor caia
+            print(f"Erro no login: {e}")
+            return jsonify({"error": "Erro interno no servidor."}), 500
--- src/Infrastructure/Model/user.py
+++ src/Infrastructure/Model/user.py
@@ -7,7 +7,7 @@ class User(db.Model):
     cnpj = db.Column(db.String(18), unique=True, nullable=False)  # CNPJ com formato XX.XXX.XXX/XXXX-XX
     email = db.Column(db.String(100), unique=True, nullable=False)
     celular = db.Column(db.String(15), nullable=False)  # Formato +55XXXXXXXXXX
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     status = db.Column(db.String(10), default='Inativo', nullable=False)  # 'Ativo' ou 'Inativo'
     activation_code = db.Column(db.String(4), nullable=True)  # Código de 4 dígitos para ativação
 
