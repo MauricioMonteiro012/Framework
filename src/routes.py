dc7284519b804771750d8f7dad00e7b3159d0452
Gabrielly Soares Marinho <2403430@salas.aulas>
2026-04-28 19:51:49 -0300

    Subindo rotas

--- src/routes.py
+++ src/routes.py
@@ -1,4 +1,5 @@
-from src.Application.Controllers.user_controller import UserController
+from src.Application.Controllers.user_controller import UserController 
+from src.Application.Controllers.product_controller import ProductController
 from flask import jsonify, make_response
 
 def init_routes(app):    
@@ -13,7 +14,6 @@ def init_routes(app):
             "mensagem": "API - OK; Docker - Up",
         }), 200)
     
-    # endpoints related to sellers (mini mercados)
     @app.route('/api/sellers', methods=['POST'])
     def register_seller():
         # cria um novo vendedor; dados são validados no controller
@@ -24,5 +24,22 @@ def init_routes(app):
         # ativa vendedor com código recebido via WhatsApp
         return UserController.activate_user()
     
+    @app.route('/api/auth/login', methods=['POST'])
+    def login():
+        return UserController.login_user()   
+
+    @app.route('/api/products', methods=['POST'])
+    def cad_products():
+        # cadastrar produtos, apenas vendedores ativos podem cadastrar
+        return ProductController.cad_products()
+    
+    @app.route('/api/products', methods=['GET'])
+    def list_products():
+        # lista todos os produtos cadastrados
+        return ProductController.list_products()
+      
+    @app.route('/api/user/update', methods=['PUT'])
+    def update_user_route():
+        return UserController.update_profile()
     
 
